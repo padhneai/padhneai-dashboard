679111044b34bd7aac6eae0be83e79bd14c867f9
codewithsamir <ajaction05@gmail.com>
2025-09-08 17:00:51 +0545

    update

--- src/config/apiClient.ts
+++ src/config/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create reusable Axios client
 const  apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://backend-production-f97b.up.railway.app/api",
   timeout: 10000,
 });
   // || "https://backend-production-f97b.up.railway.app/api",
